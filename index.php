77f84dd208320a81e5902163a1cecccad91f433f
AJJR <ash@shastrix.com>
2015-07-11 20:13:02 +0100

    set up index page with header

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Plumbery Plus - Kidderminster based heating and gas engineer</title>
+		<meta name="description" content="Established in 2002, Plumbery Plus offers a reliable, friendly and efficient service at competitive rates.">
+		<meta name="viewport" content="width=device-width">
+		<link rel="stylesheet" type="text/css" href="styles.css">
+	</head>
+
+	<body>
+		<div id="container">
+			<header>
+				<h1>Plumbery Plus Ltd</h1>
+				<nav>
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#">Services</a></li>
+						<li><a href="#">Gallery</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</nav>
+			</header>
+		</div>
+	</body>
+</html>
